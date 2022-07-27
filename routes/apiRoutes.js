8c3da9f9e65655e216acfed74230096fef8bfbca
Ron Crawford <roncrawford1300@gmail.com>
2022-07-27 05:05:16 -0400

    Added store.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const store = require('../db/store');
 
 router.get('/notes', (req, res) => {
     res.json('get!');
 });
 
 router.post('/notes', (req, res) => {
-    res.json('post!')
+    store.addNote();
+    res.json('post!');
 });
 
 module.exports = router;
